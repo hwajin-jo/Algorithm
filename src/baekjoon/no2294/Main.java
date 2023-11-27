66621102355f2aa9f0fcd363ee7d0e1b638184d0
hwajin-jo <wa8784@naver.com>
2023-11-27 13:47:57 +0900

    2023/11/27

--- src/baekjoon/no2294/Main.java
+++ src/baekjoon/no2294/Main.java
@@ -10,26 +10,26 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        int[] coin = new int[n + 1];
-        int[] d = new int[100001];
-        Arrays.fill(d, 100001);
+        int[] dp = new int[100001];
+        Arrays.fill(dp, 100001);
 
-        for (int i = 1; i <= n; i++) {
-            coin[i] = sc.nextInt();
-            if (coin[i] <= k)
-                d[coin[i]] = 1;
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+            dp[coins[i]] = 1;
         }
 
-        for (int i = 1; i <= k; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i - coin[j] >= 0)
-                    d[i] = Math.min(d[i], d[i - coin[j]] + 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (j - coins[i] >= 0) {
+                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+                }
             }
         }
 
-        if (d[k] == 100001)
+        if (dp[k] == 100001)
             System.out.println(-1);
         else
-            System.out.println(d[k]);
+            System.out.println(dp[k]);
     }
 }
