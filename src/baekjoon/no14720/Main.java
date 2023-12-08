a618ba53d754edc8937dc9a50ce2f128a00c6612
hwajin-jo <wa8784@naver.com>
2023-12-08 11:01:23 +0900

    2023/12/08

---
+++ src/baekjoon/no14720/Main.java
@@ -0,0 +1,32 @@
+package baekjoon.no14720;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int total = 0;
+        int milk = 0;
+        for (int i = 0; i < n; i++) {
+            if (milk == arr[i]) {
+                total++;
+                milk++;
+            }
+
+            if (milk == 3) {
+                milk = 0;
+            }
+        }
+
+        System.out.println(total);
+    }
+}
