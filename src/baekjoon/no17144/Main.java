dfc1b6b7d2ec05b5bfadb8f5beed00b664f01292
hwajin-jo <wa8784@naver.com>
2024-01-19 13:52:19 +0900

    2024/01/19

--- src/baekjoon/no17144/Main.java
+++ src/baekjoon/no17144/Main.java
@@ -1,10 +1,12 @@
 package baekjoon.no17144;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
+    static ArrayList<Integer> cleaner;
     static Queue<Dust> q;
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
@@ -16,29 +18,27 @@ public class Main {
         int c = sc.nextInt();
         int t = sc.nextInt();
 
+        cleaner = new ArrayList<>();
         board = new int[r][c];
 
-        int cleaner = -1;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 board[i][j] = sc.nextInt();
-                if (cleaner == -1 && board[i][j] == -1) {
-                    cleaner = i;
+                if (board[i][j] == -1) {
+                    cleaner.add(i);
                 }
             }
         }
 
-        q = new LinkedList<>();
         while (t-- > 0) {
             // 미세먼지 정보 저장
             findDust(r, c, board);
 
             // 미세먼지 확산
-            spreadDust(q, r, c);
+            spreadDust(r, c);
 
             // 공기청전기 작동
-           operating(cleaner, r, c);
-
+           operating(r, c);
         }
 
         int ans = 0;
@@ -53,18 +53,22 @@ public class Main {
     }
 
     static void findDust(int r, int c, int[][] board) {
+
+        q = new LinkedList<>();
+
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (board[i][j] > 0) {
-                    q.add(new Dust(i, r, board[i][j]));
-                }
+                if (board[i][j] == -1 || board[i][j] == 0) continue;
+                q.add(new Dust(i, j, board[i][j]));
             }
         }
     }
 
-    static void spreadDust(Queue<Dust> q, int r, int c) {
+    static void spreadDust(int r, int c) {
         while (!q.isEmpty()) {
             Dust now = q.poll();
+
+            if (now.w < 5) continue;
 
             int cnt = 0;
             int dust = now.w / 5;
@@ -75,49 +79,49 @@ public class Main {
                 if (nr < 0 || nc < 0 || nr >= r || nc >= c) continue;
                 if (board[nr][nc] == -1) continue;
                 board[nr][nc] += dust;
-                cnt++;
+                ++cnt;
             }
             board[now.r][now.c] -= dust * cnt;
         }
     }
 
-    static void operating(int cleaner, int r, int c) {
-        int top = cleaner;
-        int down = cleaner + 1;
+    static void operating(int r, int c) {
+        int top = cleaner.get(0);
+        int down = cleaner.get(1);
 
-        // 시계 방향
-        for (int i = top - 1; i > 0; i--) {
-            board[i][0] = board[i - 1][0];
+        // 반시계 방향
+        for (int x = top - 1; x > 0; x--) {
+            board[x][0] = board[x - 1][0];
         }
 
-        for (int i = 1; i < c - 1; i++) {
-            board[0][i] = board[0][i + 1];
+        for (int y = 0; y < c - 1; y++) {
+            board[0][y] = board[0][y + 1];
         }
 
-        for (int i = 0; i < top; i++) {
-            board[i][c - 1] = board[i + 1][c - 1];
+        for (int x = 0; x < top; x++) {
+            board[x][c - 1] = board[x + 1][c - 1];
         }
 
-        for (int i = c - 1; i > 1; i--) {
-            board[top][i] = board[top][i - 1];
+        for (int y = c - 1; y > 1; y--) {
+            board[top][y] = board[top][y - 1];
         }
         board[top][1] = 0;
 
-        // 반시계 방향
-        for (int i = down + 1; i < r - 1; i++) {
-            board[i][0] = board[i + 1][0];
+        // 시계 방향
+        for (int x = down + 1; x < r - 1; x++) {
+            board[x][0] = board[x + 1][0];
         }
 
-        for (int i = 0; i < c - 1; i++) {
-            board[r - 1][i] = board[r - 1][i + 1];
+        for (int y = 0; y < c - 1; y++) {
+            board[r - 1][y] = board[r - 1][y + 1];
         }
 
-        for (int i = r - 1; i > down; i--) {
-            board[i][c - 1] = board[i - 1][c - 1];
+        for (int x = r - 1; x > down; x--) {
+            board[x][c - 1] = board[x - 1][c - 1];
         }
 
-        for (int i = c - 1; i > 1; i--) {
-            board[down][i] = board[down][i - 1];
+        for (int y = c - 1; y > 1; y--) {
+            board[down][y] = board[down][y - 1];
         }
         board[down][1] = 0;
     }
