b416d1e6fc59321e57e88b59cc2bc31ef9e9bb99
hwajin-jo <wa8784@naver.com>
2024-01-22 16:11:58 +0900

    2024/01/22

--- src/baekjoon/no1916/Main.java
+++ src/baekjoon/no1916/Main.java
@@ -3,15 +3,10 @@ package baekjoon.no1916;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static ArrayList<Node>[] graph;
-    static boolean[] visited;
-    static int ans = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,10 +15,18 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+        final int INF = 1000000000;
+
+        int[][] graph = new int[n + 1][n + 1];
+        int[] cost = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            cost[i] = INF;
+
+            for (int j = 0; j <= n; j++) {
+                graph[i][j] = INF;
+            }
         }
 
         for (int i = 0; i < m; i++) {
@@ -33,7 +36,9 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            graph[a].add(new Node(b, w));
+           if (graph[a][b] > w) {
+               graph[a][b] = w;
+           }
         }
 
         st = new StringTokenizer(br.readLine());
@@ -41,32 +46,28 @@ public class Main {
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        solve(start, end, 0);
-        System.out.println(ans);
-    }
+        cost[start] = 0;
 
-    static void solve(int start, int end, int sum) {
-        if (start == end) {
-            ans = Math.min(ans, sum);
-            return;
-        }
+        for (int i = 1; i <= n; i++) {
+            int min = INF;
+            int minIndex = -1;
+            for (int j = 1; j <= n; j++) {
+                if (min > cost[j] && !visited[j]) {
+                    min = cost[j];
+                    minIndex = j;
+                }
+            }
 
-        for (int i = 0; i < graph[start].size(); i++) {
-            if (!visited[start]) {
-                visited[start] = true;
-                Node node = graph[start].get(i);
-                solve(node.v, end, sum + node.w);
-                visited[start] = false;
+            if (minIndex == -1) break;
+            visited[minIndex] = true;
+
+            for (int j = 1; j <= n; j++) {
+                if (cost[j] > cost[minIndex] + graph[minIndex][j]) {
+                    cost[j] = cost[minIndex] + graph[minIndex][j];
+                }
             }
         }
-    }
-}
 
-class Node {
-    int v, w;
-
-    public Node(int v, int w) {
-        this.v = v;
-        this.w = w;
+        System.out.println(cost[end]);
     }
 }
