883b8201d307a8b7e0b5818034d5bc0ef1b85a06
hwajin-jo <wa8784@naver.com>
2024-02-05 15:13:45 +0900

    2024/02/05

---
+++ src/baekjoon/no1965/Main.java
@@ -0,0 +1,36 @@
+package baekjoon.no1965;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int max = -1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+}
