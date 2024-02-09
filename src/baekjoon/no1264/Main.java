1bed4a18f799bfb61feb9741ba9707c84e26c379
hwajin-jo <wa8784@naver.com>
2024-02-09 14:51:39 +0900

    2024/02/09

---
+++ src/baekjoon/no1264/Main.java
@@ -0,0 +1,43 @@
+package baekjoon.no1264;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] moeum = {'a', 'e', 'i', 'o', 'u'};
+
+        while (true) {
+            String input = br.readLine();
+
+            if (input.equals("#"))
+                break;
+
+            input = input.replace(" ", "");
+//            input = input.replace(",", "");
+//            input = input.replace(".", "");
+//            input = input.replace("!", "");
+//            input = input.replace("\\?", "");
+
+            input = input.toLowerCase();
+
+            int cnt = 0;
+            for (int i = 0; i < input.length(); i++) {
+                char data = input.charAt(i);
+
+                for (int j = 0; j < moeum.length; j++) {
+                    if (data == moeum[j]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+
+            System.out.println(cnt);
+        }
+    }
+}
