c11e2dd2931c7deaafa1f4f8e5f88541da06cf22
hwajin-jo <wa8784@naver.com>
2024-02-16 16:58:50 +0900

    2024/02/16

--- src/baekjoon/no10798/Main.java
+++ src/baekjoon/no10798/Main.java
@@ -7,18 +7,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        char[][] word = new char[5][];
+        char[][] word = new char[5][15];
 
+        int max = 0;
         for (int i = 0; i < 5; i++) {
             String input = sc.next();
-            word[i] = new char[input.length()];
+            max = Math.max(max, input.length());
             for (int j = 0; j < input.length(); j++) {
                 word[i][j] = input.charAt(j);
             }
         }
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < word[i].length; j++) {
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (word[j][i] == '\0') continue;
                 System.out.print(word[j][i]);
             }
         }
