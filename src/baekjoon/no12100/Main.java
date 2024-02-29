6feef2dbe56cef32b1863b1b018149f220788d94
hwajin-jo <wa8784@naver.com>
2024-02-29 16:16:31 +0900

    2024/02/29

--- src/baekjoon/no12100/Main.java
+++ src/baekjoon/no12100/Main.java
@@ -6,14 +6,15 @@ public class Main {
 
     static int n;
     static int ans = 0;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+
+    static int[][] map;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
 
-        int[][] map = new int[n][n];
+        map = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -21,69 +22,153 @@ public class Main {
             }
         }
 
-        solve(0, map);
+        solve(0);
 
         System.out.println(ans);
     }
 
-    static void solve(int cnt, int[][] map) {
-
-        int[][] newMap = copyMap(map);
-
+    static void solve(int cnt) {
         if (cnt == 5) {
             ans = Math.max(ans, getMaxBlock(map));
             return;
         }
 
-        for (int d = 0; d < 4; d++) {
-            move(d, newMap);
-            solve(cnt + 1, newMap);
-        }
-
-    }
-
-    static int[][] copyMap(int[][] map) {
         int[][] temp = new int[n][n];
+        copyMap(temp, map);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                temp[i][j] = map[i][j];
+        for (int d = 0; d < 4; d++) {
+            switch (d) {
+                case 0:
+                    up(map);
+                    break;
+                case 1:
+                    down(map);
+                    break;
+                case 2:
+                    left(map);
+                    break;
+                case 3:
+                    right(map);
+                    break;
             }
+            solve(cnt + 1);
+            copyMap(map, temp);
         }
-
-        return temp;
     }
 
-    static int[][] move(int d, int[][] map) {
-        boolean[][] check = new boolean[n][n];
-
+    static void up(int[][] map) {
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int nr = i + dr[d];
-                int nc = j + dc[d];
+            int idx = 0;
+            for (int j = 1; j < n; j++) {
+                int prev = map[idx][i];
+                int now = map[j][i];
 
-                if (nr < 0 || nc < 0 || nr >= n || nc >= n) continue;
-                if (check[nr][nc]) continue;
-                if (map[nr][nc] > 0 && map[nr][nc] == map[i][j]) {
-                    map[nr][nc] = map[i][j] * 2;
-                    map[i][j] = 0;
-                    check[nr][nc] = true;
-                } else if (map[nr][nc] == 0){
-                    map[nr][nc] = map[i][j];
-                    map[i][j] = 0;
-                    check[nr][nc] = true;
+                if (now != 0) {
+                    if (prev == 0) {
+                        map[idx][i] = now;
+                        map[j][i] = 0;
+                    } else if (prev == now) {
+                        map[idx][i] *= 2;
+                        map[j][i] = 0;
+                        idx++;
+                    } else {
+                        idx++;
+                        map[idx][i] = map[j][i];
+                        if (idx != j) map[j][i] = 0;
+                    }
                 }
             }
         }
-
-        return map;
     }
 
-    static int getMaxBlock(int[][] board) {
-        int max = 0;
+    static void down(int[][] map) {
+        for (int i = 0; i < n; i++) {
+            int idx = n - 1;
+            for (int j = n - 2; j >= 0; j--) {
+                int prev = map[idx][i];
+                int now = map[j][i];
+
+                if (now != 0) {
+                    if (prev == 0) {
+                        map[idx][i] = now;
+                        map[j][i] = 0;
+                    } else if (prev == now) {
+                        map[idx][i] *= 2;
+                        map[j][i] = 0;
+                        idx--;
+                    } else {
+                        idx--;
+                        map[idx][i] = map[j][i];
+                        if (idx != j) map[j][i] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    static void left(int[][] map) {
+        for (int i = 0; i < n; i++) {
+            int idx = 0;
+            for (int j = 1; j < n; j++) {
+                int prev = map[i][idx];
+                int now = map[i][j];
+
+                if (now != 0) {
+                    if (prev == 0) {
+                        map[i][idx] = now;
+                        map[i][j] = 0;
+                    } else if (prev == now) {
+                        map[i][idx] *= 2;
+                        map[i][j] = 0;
+                        idx++;
+                    } else {
+                        idx++;
+                        map[i][idx] = map[i][j];
+                        if (idx != j) map[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    static void right(int[][] map) {
+        for (int i = 0; i < n; i++) {
+            int idx = n - 1;
+            for (int j = n - 2; j >= 0; j--) {
+                int prev = map[i][idx];
+                int now = map[i][j];
+
+                if (now != 0) {
+                    if (prev == 0) {
+                        map[i][idx] = now;
+                        map[i][j] = 0;
+                    } else if (prev == now) {
+                        map[i][idx] *= 2;
+                        map[i][j] = 0;
+                        idx--;
+                    } else {
+                        idx--;
+                        map[i][idx] = map[i][j];
+                        if (idx != j) map[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    static void copyMap(int[][] to, int[][] from) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                max = Math.max(max, board[i][j]);
+                to[i][j] = from[i][j];
+            }
+        }
+    }
+
+    static int getMaxBlock(int[][] map) {
+        int max = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.max(max, map[i][j]);
             }
         }
         return max;
