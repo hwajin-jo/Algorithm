8c433e204d86ba6e3b4e681636b2af05aae83e1e
hwajin-jo <wa8784@naver.com>
2024-03-29 15:06:42 +0900

    2024/03/29

---
+++ src/baekjoon/no21921/Main.java
@@ -0,0 +1,50 @@
+package baekjoon.no21921;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int i = 0; i < x; i++) {
+            sum += arr[i];
+        }
+
+        int max = sum;
+        int cnt = 1;
+        for (int i = x; i < n; i++) {
+            sum -= arr[i - x];
+            sum += arr[i];
+
+            if (sum > max) {
+                max = sum;
+                cnt = 1;
+            } else if (sum == max) {
+                cnt++;
+            }
+        }
+
+        if (max == 0) {
+            System.out.println("SAD");
+        } else {
+            System.out.println(max);
+            System.out.println(cnt);
+        }
+    }
+}
