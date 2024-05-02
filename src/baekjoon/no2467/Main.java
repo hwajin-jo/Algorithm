66e3c9b8deac344ae0602938ab6982fd491ae98f
hwajin-jo <wa8784@naver.com>
2024-05-02 13:34:55 +0900

    2024/05/02

---
+++ src/baekjoon/no2467/Main.java
@@ -0,0 +1,54 @@
+package baekjoon.no2467;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = n - 1;
+
+        long ans = Long.MAX_VALUE;
+        int near_start = 0;
+        int near_end = 0;
+        while (start < end) {
+            long mid = arr[start] + arr[end];
+
+            if (mid == 0) {
+                near_start = start;
+                near_end = end;
+                break;
+            } else if (mid < 0) {
+                if (Math.abs(ans) > Math.abs(mid)) {
+                    ans = mid;
+                    near_start = start;
+                    near_end = end;
+                }
+                start++;
+            } else {
+                if (Math.abs(ans) > Math.abs(mid)) {
+                    ans = mid;
+                    near_start = start;
+                    near_end = end;
+                }
+                end--;
+            }
+        }
+
+        System.out.println(arr[near_start] + " " + arr[near_end]);
+    }
+}
