bf4d656c0dafeac14909b51c2e49191835a3bbc2
hwajin-jo <wa8784@naver.com>
2024-05-09 13:13:05 +0900

    2024/05/09

---
+++ src/baekjoon/no9375/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.no9375;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int ans = 1;
+            Map<String, Integer> map = new HashMap<>();
+            while (N-- > 0) {
+                String name = sc.next();
+                String category = sc.next();
+
+                map.put(category, map.getOrDefault(category, 0) + 1);
+            }
+
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                ans *= (entry.getValue() + 1);
+            }
+
+
+            System.out.println(ans - 1);
+        }
+    }
+}
