ad9698498ade987de1e2075ee3c9464080aef886
hwajin-jo <wa8784@naver.com>
2024-05-15 22:18:30 +0900

    2024/05/15

---
+++ src/baekjoon/no22233/Main.java
@@ -0,0 +1,40 @@
+package baekjoon.no22233;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            set.add(st.nextToken());
+        }
+
+        for (int i = 0; i < m ; i++) {
+            st = new StringTokenizer(br.readLine());
+            String[] input = st.nextToken().split(",");
+
+            for (int j = 0; j < input.length; j++) {
+                if (set.contains(input[j])) {
+                    set.remove(input[j]);
+                }
+            }
+
+            bw.write(set.size() + " \n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
