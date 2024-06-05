f6dc389ecf4b0b046685289a3394b75ea802982b
hwajin-jo <wa8784@naver.com>
2024-06-05 10:10:01 +0900

    2024/06/05

--- src/baekjoon/no9657/Main.java
+++ src/baekjoon/no9657/Main.java
@@ -10,18 +10,17 @@ public class Main {
 
         int n = sc.nextInt();
 
-        int[] dp = new int[n + 1];
+        int[] dp = new int[1001];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 1;
         dp[4] = 1;
         for (int i = 5; i <= n; i++) {
+            dp[i] = 2;
             if (dp[i - 4] % 2 == 0 ||
                 dp[i - 3] % 2 == 0 ||
                 dp[i - 1] % 2 == 0) {
                 dp[i] = 1;
-            } else {
-                dp[i] = 2;
             }
         }
 
