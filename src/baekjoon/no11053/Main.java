0d8746c9eca62397c00c9ff9ebe1c427b5647b78
hwajin-jo <wa8784@naver.com>
2024-06-18 14:45:02 +0900

    2024/06/18

--- src/baekjoon/no11053/Main.java
+++ src/baekjoon/no11053/Main.java
@@ -8,20 +8,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-
         int[] arr = new int[n + 1];
-        int[] d = new int[n + 1];
+
         for (int i = 1; i <= n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        int max = 0;
+        int[] dp = new int[n + 1];
+        int max = 1;
         for (int i = 1; i <= n; i++) {
-            d[i] = 1;
+            dp[i] = 1;
             for (int j = 1; j < i; j++) {
-                if (arr[j] > arr[i]) continue;
-                d[i] = Math.max(d[i], d[j] + 1);
-                max = Math.max(d[i], max);
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(dp[i], max);
+                }
             }
         }
 
