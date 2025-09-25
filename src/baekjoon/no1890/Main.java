9d8a1ce19474ac4b024b5f94abedece04674d7c4
hwajin-jo <wa8784@naver.com>
2025-09-25 15:44:57 +0900

    2025/09/25

--- src/baekjoon/no1890/Main.java
+++ src/baekjoon/no1890/Main.java
@@ -1,16 +1,13 @@
 package baekjoon.no1890;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
-    static int N;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        N = sc.nextInt();
+        int N = sc.nextInt();
         int[][] arr = new int[N][N];
         long[][] dp = new long[N][N];
 
@@ -20,42 +17,29 @@ public class Main {
             }
         }
 
+        int[] dr = {1, 0};
+        int[] dc = {0, 1};
         dp[0][0] = 1;
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (i == N - 1 && j == N - 1) break;
-                if (dp[i][j] != 0) {
-                    int nextD = i + arr[i][j];
-                    if (nextD < N) {
-                        dp[nextD][j] += dp[i][j];
-                    }
 
-                    int nextR = j + arr[i][j];
-                    if (nextR < N) {
-                        dp[i][nextR] += dp[i][j];
-                    }
+                if (dp[i][j] == 0) continue;
+
+                int step = arr[i][j];
+
+                for (int d = 0; d < 2; d++) {
+                    int nr = i + dr[d] * step;
+                    int nc = j + dc[d] * step;
+
+                    if (nr >= N || nc >= N) continue;
+
+                    dp[nr][nc] += dp[i][j];
                 }
-
             }
         }
 
         System.out.println(dp[N - 1][N - 1]);
-    }
-
-    public static boolean isValid(int nx, int ny) {
-        return 0 <= nx && nx < N && 0 <= ny && ny < N;
-    }
-}
-
-class Point {
-    int x;
-    int y;
-    int step;
-
-    public Point(int x, int y, int step) {
-        this.x = x;
-        this.y = y;
-        this.step = step;
     }
 }
