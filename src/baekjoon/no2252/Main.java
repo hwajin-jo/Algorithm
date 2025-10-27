aa2e126ea38d2e7b434a96631322fab5ab80db95
hwajin-jo <wa8784@naver.com>
2025-10-27 22:48:02 +0900

    2025/10/27

--- src/baekjoon/no2252/Main.java
+++ src/baekjoon/no2252/Main.java
@@ -1,45 +1,60 @@
 package baekjoon.no2252;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        List<Integer>[] list = new List[n + 1];
-        int[] indgree = new int[n + 1];
-        int[] check = new int[n + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++) {
-            list[i] = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < m; i++) {
-            int src = sc.nextInt();
-            int dist = sc.nextInt();
-            list[src].add(dist);
-            indgree[dist]++;
-        }
-
+        // indegree 정보 입력
         Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int src = Integer.parseInt(st.nextToken());
+            int dist = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++) {
-            if (indgree[i] == 0) q.add(i);
+            graph.get(src).add(dist);
+            indegree[dist]++;
         }
 
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            check[now] = 1;
-            System.out.print(now + " ");
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0)
+                q.add(i);
+        }
 
-            for (int next : list[now]) {
-                if (check[next] == 1) continue;
-                indgree[next]--;
-                if (indgree[next] == 0) q.add(next);
+        StringBuilder sb = new StringBuilder();
+        while (!q.isEmpty()) {
+            int src = q.poll();
+
+            sb.append(src).append(" ");
+
+            for (int dist : graph.get(src)) {
+                if (indegree[dist] > 0)
+                    indegree[dist]--;
+
+                if (indegree[dist] == 0)
+                    q.add(dist);
             }
         }
+
+        System.out.println(sb);
     }
 }
