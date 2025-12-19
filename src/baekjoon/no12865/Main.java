28fc8ac0124646853fbd09a56f0ad576f4df05ff
hwajin-jo <wa8784@naver.com>
2025-12-19 13:39:22 +0900

    2025/12/19

--- src/baekjoon/no12865/Main.java
+++ src/baekjoon/no12865/Main.java
@@ -1,26 +1,29 @@
 package baekjoon.no12865;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         int[] dp = new int[K + 1];
-
         for (int i = 0; i < N; i++) {
-            int W = sc.nextInt();
-            int V = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
-            for (int j = K; j >= W; j--) {
-                dp[j] = Math.max(dp[j - W] + V, dp[j]);
+            for (int j = K; j >= w; j--) {
+                dp[j] = Math.max(dp[j], dp[j - w] + v);
             }
         }
-
 
         System.out.println(dp[K]);
     }
