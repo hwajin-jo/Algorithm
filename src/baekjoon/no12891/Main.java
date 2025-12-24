3de9688ced8e47e42760698d0684c64ae9d7aae0
hwajin-jo <wa8784@naver.com>
2025-12-24 11:31:38 +0900

    2025/12/24

--- src/baekjoon/no12891/Main.java
+++ src/baekjoon/no12891/Main.java
@@ -1,6 +1,9 @@
 package baekjoon.no12891;
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -11,48 +14,58 @@ public class Main {
         int S = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        String input = st.nextToken();
-        char[] arr = input.toCharArray();
+        char[] DNA = br.readLine().toCharArray();
 
         st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int g = Integer.parseInt(st.nextToken());
-        int t = Integer.parseInt(st.nextToken());
+
+        int aCnt = Integer.parseInt(st.nextToken());
+        int cCnt = Integer.parseInt(st.nextToken());
+        int gCnt = Integer.parseInt(st.nextToken());
+        int tCnt = Integer.parseInt(st.nextToken());
 
         int ans = 0;
-        int aCnt = 0, cCnt = 0, gCnt = 0, tCnt = 0;
+
+        int a = 0;
+        int c = 0;
+        int g = 0;
+        int t = 0;
+
         int left = 0;
         int right = 0;
 
         while (right < S) {
             if (right - left < P) {
-                if (arr[right] == 'A') {
-                    aCnt++;
+                if (DNA[right] == 'A') {
+                    a++;
                     right++;
-                } else if (arr[right] == 'C') {
-                    cCnt++;
+                } else if (DNA[right] == 'C') {
+                    c++;
                     right++;
-                } else if (arr[right] == 'G') {
-                    gCnt++;
+                } else if (DNA[right] == 'G') {
+                    g++;
                     right++;
-                } else if (arr[right] == 'T') {
-                    tCnt++;
+                } else if (DNA[right] == 'T') {
+                    t++;
                     right++;
                 }
+
             } else if (right - left == P) {
-                if (arr[left] == 'A') aCnt--;
-                else if (arr[left] == 'C') cCnt--;
-                else if (arr[left] == 'G') gCnt--;
-                else if (arr[left] == 'T') tCnt--;
+                if (DNA[left] == 'A') {
+                    a--;
+                } else if (DNA[left] == 'C') {
+                    c--;
+                } else if (DNA[left] == 'G') {
+                    g--;
+                } else if (DNA[left] == 'T') {
+                    t--;
+                }
                 left++;
             }
 
-            if (aCnt >= a && cCnt >= c && gCnt >= g && tCnt >= t && right - left == P) ans++;
+            if (aCnt >= a && cCnt >= c && gCnt >= g && tCnt >= t) {
+                ans++;
+            }
         }
-
-
         System.out.println(ans);
     }
 }
